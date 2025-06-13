5b1c5d05e8282b831134ed64e4bd918a77b8a2d9
CecileHrt <cecilehenriet.webdesign@gmail.com>
2025-06-13 11:56:31 +0200

    Ajout des fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>1ers pas</h1>
-    </>
+    <div className="theme-base">
+      <h1 className=" text-[var(--color-mauve-clair)]">1ers pas</h1>
+
+      <h3>test font</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae
+        quasi ea ipsam labore repellat quam, alias ex minus odit sint, deserunt
+        nisi dolorum. Dolores commodi aliquam error consequatur eos.
+      </p>
+      <a href="">bknqebrn</a>
+      <button>jrggn sv</button>
+    </div>
   );
 }
 
